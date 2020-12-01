7705624c54e5a4b965854c003cdcb5c66a6c9dc8
michellejanosi <michelle.janosi@gmail.com>
2020-12-01 07:01:19 -0500

    Add word list as module

--- projects/hangman/main.py
+++ projects/hangman/main.py
@@ -1,8 +1,8 @@
 import random
 import hangman_art
+import hangman_words
 
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
 end_of_game = False
 lives = 6
