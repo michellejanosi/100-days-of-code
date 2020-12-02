c8c9b380bc1aa5aaca3b637048dc7f5abf82325b
michellejanosi <michelle.janosi@gmail.com>
2020-12-02 07:51:30 -0500

    Add encode function for cipher

---
+++ projects/caeser_cipher.py
@@ -0,0 +1,20 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(text, shift):
+    encode_text = ""
+    
+    for letter in text:
+        position = alphabet.index[letter]
+        encode_num = position + shift
+        encode_letter = alphabet[encode_num]
+        encode_text += encode_letter
+
+    print(f"Your encoded text is: {encode_text}")
+    
+encrypt(text, shift)
+
+    
