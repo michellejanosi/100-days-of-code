078d9af4c0c5635b5b105d4fddbb5e36a300b29a
michellejanosi <michelle.janosi@gmail.com>
2020-12-06 15:48:56 -0500

    Add house rules and imports

---
+++ projects/blackjack/main.py
@@ -0,0 +1,12 @@
+from art import logo
+import random
+
+# House Rules
+# The deck is unlimited in size. 
+# There are no jokers. 
+# The Jack/Queen/King all count as 10.
+# The the Ace can count as 11 or 1.
+# The cards in the list have equal probability of being drawn.
+# Cards are not removed from the deck as they are drawn.
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
