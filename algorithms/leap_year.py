1b7ca5e44abef4c71391979d4527aa2643b3ff2a
michellejanosi <michelle.janosi@gmail.com>
2020-12-06 06:51:01 -0500

    Move code into a function

--- algorithms/leap_year.py
+++ algorithms/leap_year.py
@@ -2,16 +2,16 @@
 
 # A leap year is on every year that is evenly divisible by 4 **except** every year that is evenly divisible by 100 **unless** the year is also evenly divisible by 400
 
-year = int(input("What year do you want to check? "))
-
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-           return True
-        else: 
-           return False
-    else: 
-           return True
-else:
-  print(f"{year} is not a leap year.")
+def is_leap_year(year):
+   if year % 4 == 0:
+      if year % 100 == 0:
+         if year % 400 == 0:
+            return True
+         else: 
+            return False
+      else: 
+            return True
+   else:
+   
+   return False
 
