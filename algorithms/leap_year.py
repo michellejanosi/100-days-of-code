c9040233e424df2f32cd98d195114bfc3e3c01d3
michellejanosi <michelle.janosi@gmail.com>
2020-12-06 08:58:56 -0500

    Add function to calculate number of days in a month

--- algorithms/leap_year.py
+++ algorithms/leap_year.py
@@ -3,15 +3,32 @@
 # A leap year is on every year that is evenly divisible by 4 **except** every year that is evenly divisible by 100 **unless** the year is also evenly divisible by 400
 
 def is_leap_year(year):
-   if year % 4 == 0:
-      if year % 100 == 0:
-         if year % 400 == 0:
-            return True
-         else: 
-            return False
-      else: 
-            return True
-   else:
-   
-   return False
+	if year % 4 == 0:
+		if year % 100 == 0:
+			if year % 400 == 0:
+				return True
+			else: 
+				return False
+		else: 
+			return True
+	else:
+		return False
 
+# determin the number of days in a month in a given year
+
+def days_in_month(year, month):
+	month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+	leap_year = is_leap_year(year)
+	
+	if leap_year and month == 2:
+		return 29
+	else: 
+		return 28
+	
+	return month_days[month] - 1
+
+print("Check how many days are in a given month.")
+year = int(input("Enter a year: "))
+month = int(input("What is the month: "))
+days = days_in_month(year, month)
+print(f"{days} days")
