1b95fddec3e83c3af2fc8aa4d1f3a4b60d8125df
michellejanosi <michelle.janosi@gmail.com>
2020-12-12 09:09:02 -0500

    Add finished project code

--- projects/higher_lower/main.py
+++ projects/higher_lower/main.py
@@ -1,18 +1,47 @@
-import art, data, random
+import os, art, data, random
 
-print(art.logo)
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
 
-# random choose a and b from data. store in variable
-# print compare a first random choice data 
-# print vs art
-# print against b second random choice data 
-# print and store in variable who has more followers a or b
-# compare a and b which is greater, if user chose correct, add point to score. if not, user loses and game ends
+def choose_random():
+    return random.choice(data.data)
 
-choice_a = random.choice(data.data)
-choice_b = random.choice(data.data)
+def compare(guess, choice_a, choice_b):
+    choice_a = choice_a['follower_count']
+    choice_b = choice_b['follower_count']
 
-print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}.")
-print(art.vs)
-print(f"Against B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}.")
-user_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+    if choice_a > choice_b:
+        return guess == 'A'
+    else:
+        return guess == 'B'
+
+def play_game():
+    score = 0
+    continue_playing = True
+    choice_a = choose_random()
+    choice_b = choose_random()
+    print(art.logo)
+
+    while continue_playing:
+        choice_a = choice_b
+        choice_b = choose_random()
+
+        while choice_a == choice_b:
+            choice_b = choose_random()
+
+        print(f"Compare A: {choice_a['name']}, a/an {choice_a['description']} from {choice_a['country']}")
+        print(art.vs)
+        print(f"Compare B: {choice_b['name']}, a/an {choice_b['description']} from {choice_b['country']}")
+        guess = input("Who has a higher Instagram follower count? Type 'A' or 'B' ").upper()
+        is_correct = compare(guess, choice_a, choice_b)
+
+        clear_screen()
+        print(art.logo)
+        if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}.")
+        else:
+            continue_playing = False
+            print(f"Sorry, that's wrong. Final score: {score}")
+
+play_game()
