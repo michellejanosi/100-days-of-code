ca40a83030e1c6d31de539491c3ce9eaf37ab5ab
michellejanosi <michelle.janosi@gmail.com>
2020-12-15 07:26:16 -0500

    Add folder and file to practice importing modules and packages

---
+++ projects/turtle_graphics/main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+from prettytable import PrettyTable
+
+timmy = Turtle()
+my_screen = Screen()
+my_screen.canvheight
+print(my_screen)
+my_screen.exitonclick()
+
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
