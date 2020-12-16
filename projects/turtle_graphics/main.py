ab9a4616b35000a3c75aece04932c4acafd59c40
michellejanosi <michelle.janosi@gmail.com>
2020-12-16 08:40:57 -0500

    Commented out turtle code

--- projects/turtle_graphics/main.py
+++ projects/turtle_graphics/main.py
@@ -1,16 +1,16 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 from prettytable import PrettyTable
 import pyjokes
 joke = pyjokes.get_joke('en', 'neutral')
 print(joke)
 
 
-timmy = Turtle()
-my_screen = Screen()
-my_screen.canvheight
-print(my_screen)
-my_screen.exitonclick()
+# timmy = Turtle()
+# my_screen = Screen()
+# my_screen.canvheight
+# print(my_screen)
+# my_screen.exitonclick()
 
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
