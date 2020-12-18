2ec6a85e3cbd3a5ded51e4886c914e182776145f
michellejanosi <michelle.janosi@gmail.com>
2020-12-18 07:53:59 -0500

    Add function to print question from model

---
+++ projects/quiz/main.py
@@ -0,0 +1,13 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    question_text = question['text']
+    question_answer = question['answer']
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+print(question_bank[0].question)
+print(question_bank[0].answer)
