296b2dcd516cebcca4ec6613d84af69402c8e9f1
michellejanosi <michelle.janosi@gmail.com>
2020-12-18 08:15:49 -0500

    Add functionality to dislay quiz question

--- projects/quiz/main.py
+++ projects/quiz/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -9,5 +10,5 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-print(question_bank[0].question)
-print(question_bank[0].answer)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
