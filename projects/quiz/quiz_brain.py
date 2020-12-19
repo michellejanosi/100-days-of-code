2a0bbb8d1b434294b0f774435029ab729cddbb28
michellejanosi <michelle.janosi@gmail.com>
2020-12-19 05:50:08 -0500

    Add function to check # of questions left

--- projects/quiz/quiz_brain.py
+++ projects/quiz/quiz_brain.py
@@ -4,6 +4,13 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = question_list
 
+    def still_has_questions(self, question_list):
+        number_of_questions = len(self.question_list)
+        if self.question_number < number_of_questions:
+            return True
+        else:
+            return False
+
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
