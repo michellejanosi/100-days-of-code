bf515acc11a63dc0a2fe9cd37cd2023b69c9132e
michellejanosi <michelle.janosi@gmail.com>
2020-12-22 07:07:59 -0500

    Add code for algorithm

---
+++ algorithms/duplicates.py
@@ -0,0 +1,13 @@
+# check_for_duplicates(['B', 'D', 'F', 'B', 'M', 'N', 'N']) ['B', 'N']
+
+def check_for_duplicates(items):
+    """
+    Returns a list of duplicate items from passed in list
+    """
+    duplicates = []
+    for item in items:
+        if items.count(item) > 1:
+            if item not in duplicates:
+                duplicates.append(item)
+
+    return duplicates
