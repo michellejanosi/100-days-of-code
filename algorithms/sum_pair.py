6051c6c235e9fd23dd164c7eaa0df3f842efccc2
michellejanosi <michelle.janosi@gmail.com>
2020-12-22 08:04:03 -0500

    Add starter code for algorithm

---
+++ algorithms/sum_pair.py
@@ -0,0 +1,18 @@
+'''
+sum_pairs([4,2,10,5,1], 6) # [4,2]
+sum_pairs([11,20,4,2,1,5], 100) # []
+'''
+
+
+def sum_pairs(items, number):
+    '''
+    Return first pair of numbers that sum to the number passed to the function.
+    '''
+    # variable to store pairs
+    # iterate list of items
+    # compare each number in list with next index
+    # return numbers if add up to number
+    pair = []
+    
+
+ sum_pairs([4,2,10,5,1], 6)   
