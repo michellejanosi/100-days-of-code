c55a70c551ddf7346f813fb31c6855608975ee03
michellejanosi <michelle.janosi@gmail.com>
2020-12-24 12:17:10 -0500

    Revise comments

--- algorithms/sum_pair.py
+++ algorithms/sum_pair.py
@@ -3,12 +3,13 @@ sum_pairs([4,2,10,5,1], 6) # [4,2]
 sum_pairs([11,20,4,2,1,5], 100) # []
 '''
 
+
 def sum_pairs(items, number):
     '''
     Return first pair of numbers that sum to the number passed to the function.
     '''
     # initiate variable to hold unique item from items
-    # iterate items and compare item with number
+    # iterate items
     # if number minus item equals the number, return list of pair
     # otherwise, add item to value of checked items
     # if no pair adds up to number, return empty list
