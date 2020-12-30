652f26ed20d1af3a5a90536f5f38ca4faf7e38aa
michellejanosi <michelle.janosi@gmail.com>
2020-12-30 08:01:27 -0500

    Add function to draw random colored dots

--- projects/hirst_painting/main.py
+++ projects/hirst_painting/main.py
@@ -1,4 +1,32 @@
-import colorgram
+from turtle import Turtle, Screen, color
+import random
+# import colorgram
 
-colors = colorgram.extract('image.jpg', 10)
-print(colors.Color)
+# colors = colorgram.extract('image.jpg', 30)
+# rgb_colors = []
+
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+
+#     rgb_colors.append((r, g, b))
+
+# print(rgb_colors)
+color_list = [(131, 164, 204), (228, 149, 99), (30, 44, 64), (238, 245, 242), (245, 234, 238), (166, 58, 48), (202, 135, 147), (237, 212, 85), (41, 101, 150), (135, 183, 161), (150, 62, 71), (52, 42, 45), (159, 33, 31), (219, 82, 73), (238, 165, 155), (58, 117, 99), (60, 49, 45), (173, 29, 31), (231, 163, 168), (35, 61, 56), (15, 96, 71), (33, 60, 107), (170, 188, 222), (188, 101, 111), (104, 126, 161), (14, 85, 109), (174, 200, 188), (33, 151, 211)]
+
+# 10 x 10 in size, dots are sized at 20, 50 paces apart
+turtle = Turtle()
+screen = Screen()
+
+screen.colormode(255)
+turtle.speed("fast")
+
+for _ in range(10):
+    turtle.fd(10)
+    turtle.penup()
+    turtle.dot(20, random.choice(color_list))
+    turtle.fd(50)
+    turtle.pendown()
+
+screen.exitonclick()
