e0e84b834e56c6e4604c76dd5514b4b74fbba37e
michellejanosi <michelle.janosi@gmail.com>
2021-01-01 16:01:17 -0500

    Add function to draw a line

--- projects/etch_a_sketch/main.py
+++ projects/etch_a_sketch/main.py
@@ -3,6 +3,11 @@ from turtle import Turtle, Screen
 turtle = Turtle()
 screen = Screen()
 
+def move_forward():
+    turtle.forward(10)
 
+
+screen.listen()
+screen.onkey(key="space", fun=move_forward)
 
 screen.exitonclick()
