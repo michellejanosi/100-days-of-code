178aa76b357037eeb25ec56a3b23bfd3d0e96736
michellejanosi <michelle.janosi@gmail.com>
2021-01-02 10:04:06 -0500

    Add movement functions

--- projects/etch_a_sketch/main.py
+++ projects/etch_a_sketch/main.py
@@ -7,6 +7,22 @@ def move_forward():
     turtle.forward(10)
 
 
+def move_backward():
+    pass
+
+
+def move_clockwise():
+    pass
+
+
+def move_counter_clockwise():
+    pass
+
+
+def clear_screen():
+    pass
+
+
 screen.listen()
 screen.onkey(key="w", fun=move_forward)
 
