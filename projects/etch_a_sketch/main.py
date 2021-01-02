297c50b525b42d62037c40809eba7bc40606652f
michellejanosi <michelle.janosi@gmail.com>
2021-01-02 10:09:33 -0500

    Add function comments

--- projects/etch_a_sketch/main.py
+++ projects/etch_a_sketch/main.py
@@ -4,23 +4,27 @@ turtle = Turtle()
 screen = Screen()
 
 def move_forward():
+    # key "w"
     turtle.forward(10)
 
 
 def move_backward():
+    # key "s"
     pass
 
 
 def move_clockwise():
+    # key "d"
     pass
 
 
 def move_counter_clockwise():
+    # key "a"
     pass
 
 
 def clear_screen():
-    pass
+    screen.clearscreen()
 
 
 screen.listen()
