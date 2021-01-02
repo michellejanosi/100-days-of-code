db4540210617663f53de94f4ed4f4f790ef8d1e8
michellejanosi <michelle.janosi@gmail.com>
2021-01-02 12:20:04 -0500

    Add starter code

--- projects/turtle_race/main.py
+++ projects/turtle_race/main.py
@@ -1,6 +1,9 @@
 from turtle import Turtle, Screen
-screen = Screen()
 
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(
+    title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 
 
 screen.exitonclick()
