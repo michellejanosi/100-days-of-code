ebb9694e35a057032409354839485790b3d5ccb9
michellejanosi <michelle.janosi@gmail.com>
2021-01-03 08:07:05 -0500

    Add feature to create 6 turtles

--- projects/turtle_race/main.py
+++ projects/turtle_race/main.py
@@ -1,12 +1,16 @@
 from turtle import Turtle, Screen, shape
+import random
 
 screen = Screen()
 screen.setup(width=500, height=400)
 # user_bet = screen.textinput(
 #     title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 
-turtle = Turtle(shape='turtle')
-turtle.penup()
-turtle.goto(x=-230, y=-100)
+y_coord = [-70, -40, -10, 20, 50, 80]
+for turtle_index in range(0, 6):
+    turtle = Turtle(shape='turtle')
+    turtle.penup()
+    turtle.goto(x=-230, y=y_coord[turtle_index])
 
 screen.exitonclick()
