273cc34dcfb9e5073d7c1d80e1b9e3fa4c74e2a2
michellejanosi <michelle.janosi@gmail.com>
2021-01-03 07:49:59 -0500

    Add starting turtle coord

--- projects/turtle_race/main.py
+++ projects/turtle_race/main.py
@@ -1,9 +1,12 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, shape
 
 screen = Screen()
 screen.setup(width=500, height=400)
-user_bet = screen.textinput(
-    title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+# user_bet = screen.textinput(
+#     title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 
+turtle = Turtle(shape='turtle')
+turtle.penup()
+turtle.goto(x=-230, y=-100)
 
 screen.exitonclick()
