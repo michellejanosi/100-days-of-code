5aaef4716bbd3498b13c5b0c7fcf3b97b74e37ba
michellejanosi <michelle.janosi@gmail.com>
2021-01-03 08:21:06 -0500

    Add feature for turtles to race

--- projects/turtle_race/main.py
+++ projects/turtle_race/main.py
@@ -3,15 +3,27 @@ import random
 
 screen = Screen()
 screen.setup(width=500, height=400)
-# user_bet = screen.textinput(
-#     title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+user_bet = screen.textinput(
+    title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 y_coord = [-70, -40, -10, 20, 50, 80]
+turtles = []
+is_race_on = False
 
 for turtle_index in range(0, 6):
     turtle = Turtle(shape='turtle')
     turtle.color(colors[turtle_index])
     turtle.penup()
     turtle.goto(x=-230, y=y_coord[turtle_index])
+    turtles.append(turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in turtles:
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+        
 
 screen.exitonclick()
