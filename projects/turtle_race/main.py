00f0384af670c1fc9f720915194009dde4abe917
michellejanosi <michelle.janosi@gmail.com>
2021-01-03 08:08:52 -0500

    Add color to each turtle

--- projects/turtle_race/main.py
+++ projects/turtle_race/main.py
@@ -6,10 +6,11 @@ screen.setup(width=500, height=400)
 # user_bet = screen.textinput(
 #     title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-
 y_coord = [-70, -40, -10, 20, 50, 80]
+
 for turtle_index in range(0, 6):
     turtle = Turtle(shape='turtle')
+    turtle.color(colors[turtle_index])
     turtle.penup()
     turtle.goto(x=-230, y=y_coord[turtle_index])
 
