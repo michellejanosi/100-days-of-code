366a66895e2a151177e27530895d121da7e17546
michellejanosi <michelle.janosi@gmail.com>
2021-01-14 07:06:16 -0500

    Add exercise code

--- projects/exercises.py
+++ projects/exercises.py
@@ -12,10 +12,17 @@ print(list(map(capitalize_all, my_pets)))
 #2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
 my_strings = ['a', 'b', 'c', 'd', 'e']
 my_numbers = [5, 4, 3, 2, 1]
-
+print(list(zip(my_strings, sorted(my_numbers))))
 
 #3 Filter the scores that pass over 50%
 scores = [73, 20, 65, 19, 76, 100, 88]
+def passing_score(score):
+    return score > 50
 
+print(list(filter(passing_score, scores)))
 
 #4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+def accumulator(acc, item):
+    return acc + item
+
+print(reduce(accumulator, (my_numbers + scores)))
